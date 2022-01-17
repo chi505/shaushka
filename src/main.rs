0472b42452b6dde414c61be3d864baeb04aeaefd
Patrick Dixon <patrick@patrickmdixon.com>
2022-01-17 16:15:32 -0600

    Stub webserver from actix example docs

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[get("/healthcheck")]
+async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok().body("{\"version\": \"v1.0.0\"}")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(healthcheck)
+            .service(echo)
+        })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
